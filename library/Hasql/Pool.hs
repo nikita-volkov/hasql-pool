fca5c87112f714fe86ed84c4f8887afe46de23ba
Nikita Volkov <nikita.y.volkov@mail.ru>
2020-09-20 19:34:43 +0300

    Reimplement without resource-pool

--- library/Hasql/Pool.hs
+++ library/Hasql/Pool.hs
@@ -10,17 +10,78 @@ module Hasql.Pool
 where
 
 import Hasql.Pool.Prelude
-import qualified Hasql.Connection
-import qualified Hasql.Session
-import qualified Data.Pool as ResourcePool
-import qualified Hasql.Pool.ResourcePool as ResourcePool
+import Hasql.Connection (Connection)
+import Hasql.Session (Session)
+import qualified Hasql.Connection as Connection
+import qualified Hasql.Session as Session
 
 
 -- |
 -- A pool of connections to DB.
-newtype Pool =
-  Pool (ResourcePool.Pool (Either Hasql.Connection.ConnectionError Hasql.Connection.Connection))
-  deriving (Show)
+data Pool =
+  Pool
+    Int
+    {-^ Max connections. -}
+    Int
+    {-^ Connection timeout in milliseconds. -}
+    Connection.Settings
+    {-^ Connection settings. -}
+    (TQueue ActiveConnection)
+    {-^ Queue of active connections. -}
+    (TVar Int)
+    {-^ Queue size. -}
+    (TVar Bool)
+    {-^ Flag signaling whether pool's alive. -}
+
+data ActiveConnection =
+  ActiveConnection {
+    {-| Timestamp of last use. -}
+    activeConnectionLastTimestamp :: Int,
+    activeConnectionConnection :: Connection
+  }
+
+loopCollectingGarbage :: Int -> TQueue ActiveConnection -> TVar Int -> TVar Bool -> IO ()
+loopCollectingGarbage timeout queue queueSizeVar aliveVar =
+  decide
+  where
+    decide =
+      do
+        ts <- getMillisecondsSinceEpoch
+        join $ atomically $ do
+          alive <- readTVar aliveVar
+          if alive
+            then do
+              queueSize <- readTVar queueSizeVar
+              if queueSize == 0
+                then return (sleep (ts + timeout))
+                else let
+                  collect !queueSize !list =
+                    do
+                      entry@(ActiveConnection entryTs connection) <- readTQueue queue
+                      if entryTs < ts
+                        then collect (pred queueSize) (connection : list)
+                        else do
+                          writeTQueue queue entry
+                          return (entryTs, queueSize, list)
+                  in do
+                    (ts, newQueueSize, list) <- collect queueSize []
+                    writeTVar queueSizeVar newQueueSize
+                    return (release list *> sleep ts *> decide)
+            else do
+              list <- flushTQueue queue
+              writeTVar queueSizeVar 0
+              return (release (fmap activeConnectionConnection list))
+    sleep untilTs =
+      do
+        ts <- getMillisecondsSinceEpoch
+        let
+          diff =
+            untilTs - ts
+          in if diff > 0
+            then threadDelay (diff * 1000)
+            else return ()
+    release =
+      traverse_ Connection.release
 
 -- |
 -- Settings of the connection pool. Consist of:
@@ -28,48 +89,77 @@ newtype Pool =
 -- * Pool-size.
 -- 
 -- * Timeout.   
--- An amount of time for which an unused resource is kept open.
--- The smallest acceptable value is 0.5 seconds.
+-- An amount of time in milliseconds for which the unused connections are kept open.
 -- 
 -- * Connection settings.
 -- 
 type Settings =
-  (Int, NominalDiffTime, Hasql.Connection.Settings)
+  (Int, Int, Connection.Settings)
 
 -- |
 -- Given the pool-size, timeout and connection settings
 -- create a connection-pool.
 acquire :: Settings -> IO Pool
 acquire (size, timeout, connectionSettings) =
-  fmap Pool $
-  ResourcePool.createPool acquire release stripes timeout size
-  where
-    acquire =
-      Hasql.Connection.acquire connectionSettings
-    release =
-      either (const (pure ())) Hasql.Connection.release
-    stripes =
-      1
+  do
+    queue <- newTQueueIO
+    queueSizeVar <- newTVarIO 0
+    aliveVar <- newTVarIO (size > 0)
+    return (Pool size timeout connectionSettings queue queueSizeVar aliveVar)
 
 -- |
 -- Release the connection-pool.
 release :: Pool -> IO ()
-release (Pool pool) =
-  ResourcePool.destroyAllResources pool
+release (Pool _ _ _ _ _ aliveVar) =
+  atomically (writeTVar aliveVar False)
 
 -- |
 -- A union over the connection establishment error and the session error.
 data UsageError =
-  ConnectionError Hasql.Connection.ConnectionError |
-  SessionError Hasql.Session.QueryError
+  ConnectionUsageError Connection.ConnectionError |
+  SessionUsageError Session.QueryError |
+  PoolIsReleasedUsageError
   deriving (Show, Eq)
 
 -- |
 -- Use a connection from the pool to run a session and
 -- return the connection to the pool, when finished.
-use :: Pool -> Hasql.Session.Session a -> IO (Either UsageError a)
-use (Pool pool) session =
-  fmap (either (Left . ConnectionError) (either (Left . SessionError) Right)) $
-  ResourcePool.withResourceOnEither pool $
-  traverse $
-  Hasql.Session.run session
+use :: Pool -> Session.Session a -> IO (Either UsageError a)
+use (Pool _ _ connectionSettings queue queueSizeVar aliveVar) session =
+  join $ atomically $ do
+    alive <- readTVar aliveVar
+    if alive
+      then do
+        queueSize <- readTVar queueSizeVar
+        if queueSize > 0
+          then do
+            ActiveConnection _ connection <- readTQueue queue
+            let
+              !newQueueSize =
+                succ queueSize
+              in do
+                writeTVar queueSizeVar newQueueSize
+                return (useConnectionThenPutItToQueue connection)
+          else return acquireConnectionThenUseThenPutItToQueue
+      else return (return (Left PoolIsReleasedUsageError))
+  where
+    acquireConnectionThenUseThenPutItToQueue =
+      do
+        res <- Connection.acquire connectionSettings
+        case res of
+          Left acquisitionError -> return (Left (ConnectionUsageError acquisitionError))
+          Right connection -> useConnectionThenPutItToQueue connection
+    useConnectionThenPutItToQueue connection =
+      do
+        res <- Session.run session connection
+        case res of
+          Left queryError -> do
+            Connection.release connection
+            return (Left (SessionUsageError queryError))
+          Right res -> do
+            ts <- getMillisecondsSinceEpoch
+            atomically $ do
+              writeTQueue queue (ActiveConnection ts connection)
+              modifyTVar' queueSizeVar succ
+            return (Right res)
+
