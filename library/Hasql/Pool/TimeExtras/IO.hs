bd70402fba1f8fe61e0ee7a1d9c5c21133ba39fc
Nikita Volkov <nikita.y.volkov@mail.ru>
2022-08-27 17:36:09 +0300

    Add sleepUntilInMilliseconds

--- library/Hasql/Pool/TimeExtras/IO.hs
+++ library/Hasql/Pool/TimeExtras/IO.hs
@@ -6,3 +6,11 @@ import Hasql.Pool.TimeExtras.Conversions
 getMillisecondsSinceEpoch :: IO Int
 getMillisecondsSinceEpoch =
   fmap toMilliseconds getSystemTime
+
+sleepUntilInMilliseconds :: Int -> IO ()
+sleepUntilInMilliseconds untilTs = do
+  nowTs <- getMillisecondsSinceEpoch
+  let diff = untilTs - nowTs
+  if diff > 0
+    then threadDelay (diff * 1000)
+    else return ()
