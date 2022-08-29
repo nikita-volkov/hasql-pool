2fa056f7f41b032a9e2c97087d88553b6152e00a
Nikita Volkov <nikita.y.volkov@mail.ru>
2022-08-29 14:59:27 +0300

    Format

--- library/Hasql/Pool.hs
+++ library/Hasql/Pool.hs
@@ -13,11 +13,10 @@ import qualified Hasql.Connection as Connection
 import qualified Hasql.Session as Session
 import PowerPrelude
 
--- |
--- Pool of connections to DB.
+-- | Pool of connections to DB.
 newtype Pool = Pool (GenericPool.Pool Connection.ConnectionError Connection.Connection)
 
--- |  Settings of the connection pool.
+-- | Settings of the connection pool.
 data Settings
   = Settings
       Int
@@ -28,9 +27,8 @@ data Settings
       Connection.Settings
       -- ^ Connection settings.
 
--- |
--- Given the pool-size, timeout and connection settings
--- create a connection-pool.
+-- | Given the pool-size, timeout and connection settings create a
+-- connection-pool.
 acquire :: Settings -> IO Pool
 acquire (Settings size timeout connectionSettings) =
   fmap Pool $
@@ -40,9 +38,8 @@ acquire (Settings size timeout connectionSettings) =
       (Connection.acquire connectionSettings)
       Connection.release
 
--- |
--- Release all connections in the pool.
--- Connections currently in use will get released right after finishing being used.
+-- | Release all connections in the pool. Connections currently in use will
+-- get released right after finishing being used.
 --
 -- It is ok to use the pool after releasing.
 -- In such case this function effectively resets all connections.
@@ -50,8 +47,7 @@ release :: Pool -> IO ()
 release (Pool pool) =
   GenericPool.release pool
 
--- |
--- Union over the connection establishment error and the session error.
+-- | Union over the connection establishment error and the session error.
 data UsageError
   = -- | Error during an attempt to connect.
     ConnectionUsageError Connection.ConnectionError
@@ -61,7 +57,8 @@ data UsageError
 
 -- | Use a connection from the pool to run a session and return the connection
 -- to the pool, when finished. If the session fails
--- with 'Session.ClientError' the connection will eventually get reestablished.
+-- with 'Session.ClientError'the connection will eventually get
+-- reestablished.
 use :: Pool -> Session.Session a -> IO (Either UsageError a)
 use (Pool pool) session =
   fmap repackResult $ GenericPool.use pool handler
