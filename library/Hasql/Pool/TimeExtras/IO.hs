cc186c93b23ad70a59d92b5eadd120a0e8e1823a
Nikita Volkov <nikita.y.volkov@mail.ru>
2022-10-19 15:43:53 +0300

    Ditch the no longer needed "time" stuff

--- library/Hasql/Pool/TimeExtras/IO.hs
+++
@@ -1,8 +0,0 @@
-module Hasql.Pool.TimeExtras.IO where
-
-import Hasql.Pool.Prelude
-import Hasql.Pool.TimeExtras.Conversions
-
-getMillisecondsSinceEpoch :: IO Int
-getMillisecondsSinceEpoch =
-  fmap toMilliseconds getSystemTime
