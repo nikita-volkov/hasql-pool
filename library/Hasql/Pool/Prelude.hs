53846aa4d3c225402c5c06d9e8d14316fa9de145
Nikita Volkov <nikita.y.volkov@mail.ru>
2023-12-14 10:04:47 +0300

    Modernize

--- library/Hasql/Pool/Prelude.hs
+++ library/Hasql/Pool/Prelude.hs
@@ -26,7 +26,7 @@ import Data.Either as Exports
 import Data.Fixed as Exports
 import Data.Foldable as Exports hiding (toList)
 import Data.Function as Exports hiding (id, (.))
-import Data.Functor as Exports
+import Data.Functor as Exports hiding (unzip)
 import Data.Functor.Compose as Exports
 import Data.IORef as Exports
 import Data.Int as Exports
