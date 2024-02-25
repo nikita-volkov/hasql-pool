d7162ce36551d95aaf656d1e24ec61edb3c32bc4
Nikita Volkov <nikita.y.volkov@mail.ru>
2024-02-25 17:05:59 +0300

    Extend the observability docs

--- src/library/exposed/Hasql/Pool/Observation.hs
+++ src/library/exposed/Hasql/Pool/Observation.hs
@@ -1,18 +1,27 @@
+-- | Interface for processing observations of the status of the pool.
+--
+-- Provides a flexible mechanism for monitoring the healthiness of the pool via logs and metrics without any opinionated choices on the actual monitoring technologies.
+-- Specific interpreters are encouraged to be created as extension libraries.
 module Hasql.Pool.Observation where
 
 import Hasql.Pool.Prelude
 
+-- | An observation of a change of the state of a pool.
 data Observation
-  = ConnectionObservation
+  = -- | Status of one of the pool's connections has changed.
+    ConnectionObservation
       -- | Generated connection ID.
       -- For grouping the observations by one connection.
       UUID
-      -- | Connection status that it has entered.
+      -- | Status that the connection has entered.
       ConnectionStatus
   deriving (Show, Eq)
 
+-- | Status of a connection.
 data ConnectionStatus
   = -- | Connection is being established.
+    --
+    -- This is the initial status of every connection.
     ConnectingConnectionStatus
   | -- | Connection is established and not occupied.
     ReadyForUseConnectionStatus
@@ -24,6 +33,7 @@ data ConnectionStatus
     TerminatedConnectionStatus ConnectionTerminationReason
   deriving (Show, Eq)
 
+-- | Explanation of why a connection was terminated.
 data ConnectionTerminationReason
   = -- | The age timeout of the connection has passed.
     AgingConnectionTerminationReason
